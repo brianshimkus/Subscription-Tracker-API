5b7bf69d938ddcd21a3df59b2e1e7c8b107c7d64
Brian Shimkus <brianshimkus1@gmail.com>
2025-03-20 23:58:08 -0500

    Get user subscription functionality

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -11,3 +11,21 @@ export const createSubscription = async (req, res, next) => {
 		next(e)
 	}
 }
+
+export const getUserSubscriptions = async (req, res, next) => {
+	try {
+		if (req.user !== req.params.id) {
+			const error = new Error(
+				'You are not authorized to view these subscriptions'
+			)
+			error.status = 403
+			throw error
+		}
+
+		const subscriptions = await Subscription.find({ user: req.params.id })
+
+		res.status(200).json({ success: true, data: subscriptions })
+	} catch (e) {
+		next(e)
+	}
+}
